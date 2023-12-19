e1eb23d6b8df66213009f5a061c014b0a494129c
vinnt060402 <vinnt060402@gmail.com>
2023-12-19 23:42:35 +0700

    First App Roll Dice

---
+++ lib/dice_roller.dart
@@ -0,0 +1,51 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+final randomizer = Random();
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var currentDiceRoll = randomizer.nextInt(6) + 1;
+
+  void rollDice() {
+    setState(() {
+      currentDiceRoll = randomizer.nextInt(6) + 1; // 0 + 1 <= x < 11
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          "assets/images/dice-$currentDiceRoll.png",
+          width: 200,
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+              // padding: const EdgeInsets.only(
+              //   top: 20,
+              // ),
+              foregroundColor: Colors.tealAccent,
+              backgroundColor: Colors.amber,
+              textStyle: const TextStyle(fontSize: 28)),
+          child: const Text('Roll Dice'),
+        ),
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,17 @@
+import 'package:first_app/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text("Hello World!")));
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(218, 245, 160, 149),
+        body: GradientContainer(colors: [
+          Color.fromARGB(255, 162, 130, 238),
+          Color.fromARGB(255, 145, 22, 194),
+          Color.fromARGB(255, 187, 9, 119)
+        ]),
+      ),
+    ),
+  );
 }
